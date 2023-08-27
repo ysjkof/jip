2e09535feb033806ffeadfa6d2933745940c0ba1
seongjin <77876601+ysjkof@users.noreply.github.com>
2023-08-28 00:11:01 +0900

    chore: secret 파일 경로 변경, logout이 파일을 삭제하지 않는다

--- src/auth/logout.ts
+++ src/auth/logout.ts
@@ -3,11 +3,6 @@ import { ID_AND_PASSWORD_PATH } from '../constant.js';
 import { printSuccessLogout } from '../cli/output.js';
 
 export const logout = () => {
-  fs.unlink(ID_AND_PASSWORD_PATH, (err) => {
-    if (err) {
-      console.error('로그아웃 중 문제 발생.', err);
-      return;
-    }
-    printSuccessLogout();
-  });
+  fs.writeFileSync(ID_AND_PASSWORD_PATH, '');
+  printSuccessLogout();
 };
--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,9 @@
-export const ID_AND_PASSWORD_PATH = './secret.txt';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const ID_AND_PASSWORD_PATH = `${__dirname}/secret`;
 export const LINE =
   '-----------------------------------------------------------';
