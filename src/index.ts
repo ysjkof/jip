885607005d75c382379085c34d8575c3d3db520e
seongjin <77876601+ysjkof@users.noreply.github.com>
2023-08-27 13:04:07 +0900

    chore: version 1.1.3

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const jip = new Command()
   .description(
     'CLI program for input of JINSUL ERP\n진설 ERP의 입력을 위한 CLI 프로그램.'
   )
-  .version('1.1.2', '-v, --version', '프로그램의 버전을 출력한다.')
+  .version('1.1.3', '-v, --version', '프로그램의 버전을 출력한다.')
   .helpOption('-h, --help', '도움말을 출력한다.')
   .hook('preAction', async () => {
     printLine();
